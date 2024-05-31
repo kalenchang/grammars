9393329da8c095e3ccf01b0b1a225b1706423be6
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2024-05-31 02:17:44 -0700

    Create HGtoLIG.hs

---
+++ HGtoLIG.hs
@@ -0,0 +1,12 @@
+module HGtoLIG where
+
+import Prelude
+import Data.Tree
+
+data VN = S | T | B | C | D deriving (Eq, Show)
+
+type VT = String
+
+data HGRule = Concat {mother :: VN, lefts :: [VN], daughter :: VN, rights :: [VN], label :: Int} 
+        | Wrap {mother :: VN, left :: VN, right :: VN, label :: Int}
+        | Leaf {mother :: VN, lefts :: [VT], rights :: [VT], label :: Int} deriving (Show, Eq)
