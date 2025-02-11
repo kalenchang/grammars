b41e893318445c6dd2a77db0a2070790f3374b78
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2025-02-11 15:42:57 -0800

    Update TreePrint.hs

--- TreePrint.hs
+++ TreePrint.hs
@@ -25,6 +25,7 @@ latexNode ('\n':xs) = "\\\\" ++ latexNode xs
 latexNode ('\"':xs) = latexNode xs
 latexNode ('\\':xs) = latexNode xs
 latexNode ('#':xs) = "\\#" ++ latexNode xs
+latexNode ('@':xs) = "\\ap" ++ latexNode xs
 -- latexNode (',':xs) = "\\hgs " ++ latexNode xs
 latexNode ('-':'>':xs) = "\\ra{} " ++ latexNode xs
 latexNode (x:xs) = x : latexNode xs
@@ -40,11 +41,13 @@ drawLatex (Node x ts0) = lines ("[{"++(latexNode x)++"}") ++ drawSubTrees ts0 ++
 
 -------- basic categories ---------
 -- nonterminals
-data VN = S | T | U | A | B | C | D deriving (Show, Eq)
+data VN = S | T | U | V | A | B | C | D deriving (Show, Eq)
 
 -- terminals
 type VT = String
 
+-- natural language categories
+data Cats = CP | DP | VP deriving (Show, Eq)
   
 {- -- not sure how this will work; the category of an LIG isn't nts...
 class GF t y where
