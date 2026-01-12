a11107a240dde6e1ed35777c1030cf6f1b4920b3
Kalen Chang <1614171+kalenchang@users.noreply.github.com>
2026-01-12 02:51:04 -0800

    Update PDAtoCFG.hs

--- PDAtoCFG.hs
+++ PDAtoCFG.hs
@@ -23,6 +23,8 @@ instance (Show st, Show ind) => Texable (PDAtransNT st String ind) where
     texify (Sngp a) = if a == "" then "" else "\\inbar{" ++ texify a ++ "}"
     texify PCStart = "S"
 
+-- PCT section
+
 -- these transitions are for pct (greibach translation)
 -- push/pop transitions -> becomes NT rule in CFG
 -- need a list of intermediate states (sts) that will be used to pop the stack symbols added from this transition (e)
@@ -78,10 +80,48 @@ pct' [] = []
 pct' (tr@(PDAT q u [i] q' []):ts) = (CFT (makeNTTrule tr) []):(pct' ts)
 pct' (tr@(PDAT q u [i] q' js):ts) = let (treelist, rank) = (pct' ts, length js) in
     let (lf, lb) = (take rank treelist, drop rank treelist) in
-    (CFT (makeNTrule tr (map denom lf)) ((CFT (makeTrule u) []):lf)):lb
+    (CFT (makeNTrule tr (map rtdn lf)) ((CFT (makeTrule u) []):lf)):lb
 
-denom (CFT (Branching (Trpp _ _ x _) _) _) = x
-denom (CFT (Leafing (Trpp _ _ x _) _) _) = x
+-- root denominator
+rtdn (CFT (Branching (Trpp _ _ x _) _) _) = x
+rtdn (CFT (Leafing (Trpp _ _ x _) _) _) = x
+
+-- create mu' for the new cfg
+pctmu sts (trans, int)
+    | pushInd trans == [] = [ (makeNTTrule trans, int) ]
+    | otherwise = let rank = length $ pushInd trans in
+        [ (makeNTrule trans stlist, (pctterm rank) # int) | stlist <- powerlist rank sts ]
+
+powerlist 0 bank = [[]]
+powerlist n bank = [ x:rest | x <- bank, rest <- powerlist (n-1) bank]
+
+makemupct (PDA sts syms inds _ q0 z0) mulist iota = (concatMap (pctmu sts) mulist)  -- transition rules
+                                        ++ [(Leafing (Sngp u) [u], idterm) | u <- syms]  -- lexical rules
+                                        ++ [(Branching PCStart [(Trpp q0 z0 s False)], k ^ k # iota) | s <- sts]  -- start rules
+
+pctpda4mu = lookupint $ makemupct pda4 mupda4list iotapda4
+
+-- > pdartoTex pda4r1
+-- > latexTree $ cfgtoThreeLatex $ pct pda4r1
+
+-- > pdallogdentex pda4r1 mupda4 iotapda4
+-- 
+
+{-
+TODO: make a type for "rule schemas" so that mus can just store these schemas, rather than
+instantiating each rule. that would mean some kind of data type which looks like a cfg rule,
+but stores as much data as a pda transition. so for [1X3] -> [u] [1Y2] [2Z3], it would only 
+match the 1, 3, u, X, and YZ (everything in a pda transition)
+
+OR: maybe this is a good idea(?): instead of state type st, do state type st + 1, 
+such that the 1 can match with anything ie. when checking if a (PDAtransNT st sy ind) 
+matches a (PDAtransNT (st + 1) sy ind), just return true if st + 1 is 1. ("wildcard"/variable).
+do we need to ensure the variables line up? eg. in valid CFG rules: x/y y/z, you won't have x/y z/y.
+perhaps we can just assume that any rule being looked up in mu is well-formed.
+the only downside is that when printing, eg. interpretation tables, you cant show diff wildcards.
+but checking variable bindings would be annoying. compromise: all states match any variable in
+the lookup table, but they are stored as diff variables so that they can be printed differently
+-}
 
 -------------------
 -- PCM SECTION: pda to cfg, max one
